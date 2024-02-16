0f373da093d5fa74aea9c078f384f4e61f66920d
izelenka-profinit <ivo.zelenka@z-k.cz>
2024-02-16 18:15:17 +0100

    První lekce Python

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
-MY_CONST = 3
-
+MY_CONST: int = 3
+MY_VECTOR: list = [1,2,3,4,5]
 
 def add_numbers(a: int, b: int) -> int:
     return a + b
@@ -9,7 +9,7 @@ def test_add_numbers() -> None:
     print("Running tests for add_numbers ...")
 
     assert add_numbers(1, 2) == 3
-    assert add_numbers(1, 4) == 4
+    assert add_numbers(1, 3) == 4
 
     print("All tests passed")
 
--- pokus.py
+++ pokus.py
@@ -2,7 +2,24 @@ import calc
 
 
 def main() -> None:
-    print(calc.add_numbers(1, 2))
+    sum = calc.add_numbers(2, 2)
+    print(sum)
 
+def number_print() -> None:
+    hodn_1 = str(calc.MY_VECTOR [4])
+    hodn_2 = str(calc.MY_VECTOR [2])
 
-main()
+    print (hodn_1 + ", " + hodn_2) 
+    print (calc.MY_VECTOR [4] + calc.MY_VECTOR [2])
+
+# main()
+
+# number_print()
+
+for i in range (0, 10, 1):
+    if i+1<5:
+        print(i, end = ", ")
+    elif i+1<10:
+        print(i, end = "- ")
+    else:
+        print(i)
